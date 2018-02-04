43f8945dd264cd3d55ce4ad298d75f329d26074a
Aleksandar Taseski <Aleksandar.Taseski@allocatesoftware.com>
2018-02-04 19:42:23 +0100

    http, mock data

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -20,6 +20,14 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../src/index.html')));
 
+app.get('/users', (req, res) => {
+  res.json([
+    {"id": 1, "firstName": "Aleksandar", "lastName": "Taseski", "email":"aleksandar.taseski@hotmail.com"},
+    {"id": 2, "firstName": "John", "lastName": "Smith", "email":"john.smith@hotmail.com"},
+    {"id": 3, "firstName": "James", "lastName": "Bond", "email":"james.bond@hotmail.com"}
+  ]);
+});
+
 app.listen(port, (err) => {
   if(err) {
     console.log(err); // eslint-disable-line no-console
